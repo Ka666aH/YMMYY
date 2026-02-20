224a1afde1c7c5cfbb0bd32347592fd01f04b9b7
Ka666aH <dmitryhoroshev05@mail.ru>
2026-02-20 13:30:24 +0300

    Реализовать кэширование Redis.

--- Infrastructure/Cache/RedisCache.cs
+++ Infrastructure/Cache/RedisCache.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Interfaces.RepositoryInterfaces;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 namespace Infrastructure.Cache
 {
     public class RedisCache : ICache
@@ -8,14 +9,21 @@ namespace Infrastructure.Cache
 
         public RedisCache(IDistributedCache cache) => _cache = cache;
 
-        public async Task<string> GetOrCreateAsync(string key, Func<Task<string>> factory, CancellationToken ct = default)
+        public async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, CancellationToken ct = default)
         {
             var cached = await _cache.GetStringAsync(key, ct);
-            if (cached != null) return cached;
+            if (cached != null) return JsonSerializer.Deserialize<T>(cached)!;
 
-            string newValue = await factory();
-            await _cache.SetStringAsync(key, newValue, ct);
+            var newValue = await factory();
+            await _cache.SetStringAsync(key, JsonSerializer.Serialize(newValue), ct);
             return newValue;
+
+            //var cached = await _cache.GetStringAsync(key, ct);
+            //if (cached != null) return cached;
+
+            //string newValue = await factory();
+            //await _cache.SetStringAsync(key, newValue, ct);
+            //return newValue;
         }
     }
 }
